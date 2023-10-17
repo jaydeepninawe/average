12783b1412417227f48d1e9d92a0c0721a48f559
jaydeepninawe <142102780+jaydeepninawe@users.noreply.github.com>
2023-10-17 15:37:51 +0530

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+int a ,n,i;
+int sum=0,avg=0;
+    cout<<"enter number of elements :";
+    cin>>n;
+    for (int i = 0; i < n; i++) {
+        cout<<"enter number of elements :" <<i+1;
+        cin>>}a;
+        sum+=a;
+    }
+    avg=sum/n;
+    cout<<"sum is :"<<sum;
+    cout<<"avg is :"<<avg;
+    return 0;
+    
+
+
+    
+
+    }
