9fcea534cbc6e6e0fb6c73a6bcc09035e41ee123
jaydeepninawe <142102780+jaydeepninawe@users.noreply.github.com>
2023-10-17 15:36:46 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,total=0,avg=0;
+cout<<"enter the numbers you want :";
+cin>>a;
+int b=a;
+cout<<"enter"<<a<<"elements";
+while(a>0){
+cin>>c;
+total+=c;
+a--;
+}
+avg=total/b;
+cout<<"sum is :"<<total;
+cout<<"avg is :"<<avg;
+} 
+
+
